856808b727c47a95c68c2cad4f9c3840690ebc3c
JacksonPlayz <jackson.hardaway@gmail.com>
2018-06-16 13:40:31 -0700

    Added Modder's Nightmare (unused for now)

--- src/main/java/com/cubgdev/cubga/item/ItemModderNightmare.java
+++ src/main/java/com/cubgdev/cubga/item/ItemModderNightmare.java
@@ -1,51 +1,54 @@
 package com.cubgdev.cubga.item;
 
 import com.cubgdev.cubga.CUBG;
+import com.google.common.collect.Multimap;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.client.resources.I18n;
 import net.minecraft.client.util.ITooltipFlag;
-import net.minecraft.enchantment.Enchantment;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.SharedMonsterAttributes;
+import net.minecraft.entity.ai.attributes.AttributeModifier;
+import net.minecraft.entity.ai.attributes.IAttribute;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Enchantments;
+import net.minecraft.init.SoundEvents;
 import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemSword;
+import net.minecraft.stats.StatList;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.EnumActionResult;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.SoundCategory;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraft.world.World;
 
 import javax.annotation.Nullable;
+import java.util.Collection;
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
-public class ItemCanadianFlag extends Item
-{
-    public ItemCanadianFlag() {
-        this.setUnlocalizedName("canadian_flag");
-        this.setRegistryName("canadian_flag");
+public class ItemModderNightmare extends Item {
+
+    public ItemModderNightmare() {
+        this.setUnlocalizedName("modder_nightmare");
+        this.setRegistryName("modder_nightmare");
         this.setCreativeTab(CUBG.TAB);
     }
 
     @Override
-    public void onUpdate(ItemStack itemstack, World par2World, Entity par3Entity, int par4, boolean par5)
-    {
-        if(itemstack.isItemEnchanted() == false)
-        {
+    public void onUpdate(ItemStack itemstack, World par2World, Entity par3Entity, int par4, boolean par5) {
+        if (itemstack.isItemEnchanted() == false) {
             itemstack.addEnchantment(Enchantments.KNOCKBACK, 1);
         }
     }
 
     @Override
-    public boolean hasEffect(ItemStack par1ItemStack)
-    {
+    public boolean hasEffect(ItemStack par1ItemStack) {
         return false;
-    }
-
-    @Override
-    public EntityEquipmentSlot getEquipmentSlot(ItemStack stack)
-    {
-        return EntityEquipmentSlot.HEAD;
     }
 
     @Override
@@ -58,3 +61,4 @@ public class ItemCanadianFlag extends Item
         }
     }
 }
+
