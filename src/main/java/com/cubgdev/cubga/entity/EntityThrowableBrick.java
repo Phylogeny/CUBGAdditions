06d5e5e70223b420b6cdb10fe8383c17fdd27f71
MrCrayfish <mrcrayfish@hotmail.com>
2018-07-13 01:16:24 +0930

    Updated EntityThrowableBrick to use new IDamageable

--- src/main/java/com/cubgdev/cubga/entity/EntityThrowableBrick.java
+++ src/main/java/com/cubgdev/cubga/entity/EntityThrowableBrick.java
@@ -1,9 +1,9 @@
 package com.cubgdev.cubga.entity;
 
 import com.cubgdev.cubga.init.ModItems;
-import com.mrcrayfish.guns.entity.EntityProjectile;
 import com.mrcrayfish.guns.interfaces.IDamageable;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.projectile.EntityThrowable;
@@ -13,11 +13,7 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.world.World;
 
-import java.util.Random;
-
 public class EntityThrowableBrick extends EntityThrowable {
-
-    private ItemStack brick = new ItemStack(ModItems.BRICK);
 
     public EntityThrowableBrick(World world) {
         super(world);
@@ -37,33 +33,25 @@ public class EntityThrowableBrick extends EntityThrowable {
             this.world.setEntityState(this, (byte) 3);
             this.setDead();
 
-            //if (result.typeOfHit == RayTraceResult.Type.BLOCK) {
-            //    BlockPos pos = result.getBlockPos();
-            //    IBlockState state = this.world.getBlockState(pos);
-
-            //    if (state.getBlock() instanceof IDamageable) {
-            //        IDamageable damageable = (IDamageable) state.getBlock();
-
-            //        EntityProjectile projectile = new EntityProjectile(this.world);
-            //        projectile.setDamageModifier(1F + new Random().nextInt(1)); // Damage will be random 1 to 2
-            //        this.world.spawnEntity(projectile);
-
-            //        damageable.onProjectileDamaged(this.world, state, pos, projectile);
-            //    } else {
-            //        doSpawnItem();
-            //    }
-            //} else {
-            if (result.entityHit instanceof EntityPlayer) {
+            if (result.typeOfHit == RayTraceResult.Type.BLOCK) {
+                BlockPos pos = result.getBlockPos();
+                IBlockState state = this.world.getBlockState(pos);
+                if (state.getBlock() instanceof IDamageable) {
+                    IDamageable damageable = (IDamageable) state.getBlock();
+                    damageable.onBlockDamaged(this.world, state, pos, 2);
+                } else {
+                    dropItem();
+                }
+            } else if (result.entityHit instanceof EntityLivingBase) {
                 result.entityHit.attackEntityFrom(DamageSource.causeThrownDamage(this, this.getThrower()), (float) 2);
             } else {
-                doSpawnItem();
+                dropItem();
             }
-            //}
         }
     }
 
-    private void doSpawnItem() {
-        EntityItem entityItem = new EntityItem(this.world, this.posX, this.posY, this.posZ, brick);
+    private void dropItem() {
+        EntityItem entityItem = new EntityItem(this.world, this.posX, this.posY - 0.45, this.posZ, new ItemStack(ModItems.BRICK));
         this.world.spawnEntity(entityItem);
     }
 }
