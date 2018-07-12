06a213c5e3851cdeb35d6736334b6ad01705d024
MrCrayfish <mrcrayfish@hotmail.com>
2018-07-13 02:11:13 +0930

    Updated version

--- src/main/java/com/cubgdev/cubga/Reference.java
+++ src/main/java/com/cubgdev/cubga/Reference.java
@@ -4,7 +4,7 @@ public class Reference
 {
     public static final String MOD_ID = "cubga";
     public static final String MOD_NAME = "CUBG Additions Mod";
-    public static final String MOD_VERSION = "1.2.1";
+    public static final String MOD_VERSION = "1.3.0";
     public static final String MOD_COMPATIBILITY = "[1.12.2]";
     public static final String MOD_DEPENDS = "required-after:autoreglib@[1.3-18,);required-after:cgm@[0.4.3,)";
 
