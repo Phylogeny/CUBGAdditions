28cb0d416c5242fa5b51cf17cd98a22e47e375ea
JacksonPlayz <jackson.hardaway@gmail.com>
2018-07-16 22:26:34 -0700

    Revert "Revert "Merge branch 'master' of https://github.com/JacksonPlayz/CUBGAdditions""
    This reverts commit 9f31d874002a5404022cef97d5ebee0ec6f46491.

--- src/main/java/com/cubgdev/cubga/CUBG.java
+++ src/main/java/com/cubgdev/cubga/CUBG.java
@@ -7,6 +7,7 @@ import com.cubgdev.cubga.init.RegistrationHandler;
 import com.cubgdev.cubga.network.PacketHandler;
 import com.cubgdev.cubga.proxy.CommonProxy;
 import com.cubgdev.cubga.tileentity.TileEntityBrittleBrick;
+import com.cubgdev.cubga.tileentity.TileEntityCrystalContainer;
 import com.cubgdev.cubga.tileentity.TileEntityPlayerPlush;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.ItemStack;
@@ -46,8 +47,9 @@ public class CUBG
     @Mod.EventHandler
     public void init(FMLInitializationEvent event) {
         proxy.init(event);
-        GameRegistry.registerTileEntity(TileEntityBrittleBrick.class, Reference.MOD_ID + "BrittleBrick");
-        GameRegistry.registerTileEntity(TileEntityPlayerPlush.class, Reference.MOD_ID + "PlayerPlush");
+        GameRegistry.registerTileEntity(TileEntityBrittleBrick.class, Reference.MOD_ID + ":brittle_brick");
+        GameRegistry.registerTileEntity(TileEntityPlayerPlush.class, Reference.MOD_ID + ":player_plush");
+        GameRegistry.registerTileEntity(TileEntityCrystalContainer.class, Reference.MOD_ID + ":crystal_container");
     }
 
     @Mod.EventHandler
