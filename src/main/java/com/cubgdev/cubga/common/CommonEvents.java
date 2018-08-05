d73b77849e1a2d162194351fe60279b904965e65
Mastef_Chief <mastefchief@gmail.com>
2018-08-05 19:03:05 -0400

    started development on DRP State

--- src/main/java/com/cubgdev/cubga/common/CommonEvents.java
+++ src/main/java/com/cubgdev/cubga/common/CommonEvents.java
@@ -1,10 +1,21 @@
 package com.cubgdev.cubga.common;
 
+import com.cubgdev.cubga.utils.DiscordHandler;
 import net.arikia.dev.drpc.DiscordRPC;
 import net.arikia.dev.drpc.DiscordRichPresence;
+import net.minecraft.client.Minecraft;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.scoreboard.ScorePlayerTeam;
+import net.minecraft.scoreboard.Scoreboard;
+import net.minecraft.server.MinecraftServer;
+import net.minecraftforge.fml.common.FMLCommonHandler;
+import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
 import net.minecraftforge.fml.common.network.FMLNetworkEvent;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+import org.lwjgl.Sys;
 
 /**
  * Author: MrCrayfish
@@ -13,41 +24,44 @@ public class CommonEvents {
 
     public static boolean replaceBricks = false;
 
+    DiscordHandler discordHandler = DiscordHandler.getInstance();
+
     @SubscribeEvent
     public void onServerTick(TickEvent.ServerTickEvent event) {
-        if(event.phase == TickEvent.Phase.END && replaceBricks) {
+        if (event.phase == TickEvent.Phase.END && replaceBricks) {
             replaceBricks = false;
         }
     }
 
     @SubscribeEvent
-    public void onServerJoin(FMLNetworkEvent.ClientConnectedToServerEvent event){
-
-        if(event.getManager().getRemoteAddress().toString().split("/")[0].equals("cubg.mrcrayfish.com")){
-            DiscordRichPresence rich = new DiscordRichPresence.Builder("")
-                            .setDetails("Playing on Beaver.")
-                            .setBigImage("cubgcurselogo_png", "CUBG")
-                            .setSmallImage("canada_png", "Canada")
-                            .build();
-            DiscordRPC.discordUpdatePresence(rich);
-        }
-        if(event.getManager().getRemoteAddress().toString().split("/")[0].equals("cubgau.mrcrayfish.com")){
-
-                    DiscordRichPresence rich = new DiscordRichPresence.Builder("")
-                            .setDetails("Playing on Kangaroo.")
-                            .setBigImage("cubgcurselogo_png", "CUBG")
-                            .setSmallImage("australia_png", "Australia")
-                            .build();
-            DiscordRPC.discordUpdatePresence(rich);
+    public void onClientTick(TickEvent.ClientTickEvent event) {
+        if (event.phase.equals(TickEvent.Phase.END)) {
+            if (Minecraft.getMinecraft().world != null) {
+                Scoreboard scoreboard = Minecraft.getMinecraft().world.getScoreboard();
+                ScorePlayerTeam scoreplayerteam = scoreboard.getPlayersTeam(Minecraft.getMinecraft().player.getName());
+                if (scoreplayerteam != null) {
+                    if (scoreplayerteam.getDisplayName().equals("Lobby")) {
+                    discordHandler.getDiscordRichPresence().state="In Lobby";
+                    discordHandler.updateRichPresence();
+                    }
+                }
+            }
         }
     }
 
     @SubscribeEvent
-    public void onServerLeave(FMLNetworkEvent.ClientDisconnectionFromServerEvent event){
-        DiscordRichPresence rich = new DiscordRichPresence.Builder("")
-                        .setDetails("In the Main Menu")
-                        .setBigImage("cubgcurselogo_png", "CUBG")
-                        .build();
-        DiscordRPC.discordUpdatePresence(rich);
+    public void onServerJoin(FMLNetworkEvent.ClientConnectedToServerEvent event) {
+
+        if (event.getManager().getRemoteAddress().toString().split("/")[0].equals("cubg.mrcrayfish.com")) {
+            discordHandler.setRichPresence("", "Playing on Beaver", "cubgcurselogo_png", "CUBG", "canada_png", "Canada");
+        }
+        if (event.getManager().getRemoteAddress().toString().split("/")[0].equals("cubgau.mrcrayfish.com")) {
+            discordHandler.setRichPresence("", "Playing on Kangaroo", "cubgcurselogo_png", "CUBG", "australia_png", "Australia");
+        }
+    }
+
+    @SubscribeEvent
+    public void onServerLeave(FMLNetworkEvent.ClientDisconnectionFromServerEvent event) {
+        discordHandler.setRichPresence("", "In the Main Menu", "cubgcurselogo_png", "CUBG");
     }
 }
--- src/main/java/com/cubgdev/cubga/utils/DiscordHandler.java
+++ src/main/java/com/cubgdev/cubga/utils/DiscordHandler.java
@@ -13,21 +13,38 @@ public class DiscordHandler {
         return instance;
     }
 
+    private DiscordRichPresence discordRichPresence;
+
     public void setup(){
 
         Runtime.getRuntime().addShutdownHook(new Thread(() -> {
             System.out.println("Shutting down DiscordHook.");
             DiscordRPC.discordShutdown();
         }));
-
         DiscordEventHandlers handlers = new DiscordEventHandlers.Builder().setReadyEventHandler((user) -> {}).build();
         DiscordRPC.discordInitialize("474221751119904768", handlers, true);
         DiscordRPC.discordRunCallbacks();
 
-        DiscordRPC.discordUpdatePresence(
-                new DiscordRichPresence.Builder("")
-                        .setDetails("In the Main Menu")
-                        .setBigImage("cubgcurselogo_png", "CUBG")
-                        .build());
+        setRichPresence("", "In the Main Menu", "cubgcurselogo_png", "CUBG");
     }
+    public void setRichPresence(String state, String details, String bigImage, String bigImageText){
+        discordRichPresence = new DiscordRichPresence.Builder(state)
+                .setDetails(details)
+                .setBigImage(bigImage, bigImageText)
+                .build();
+        DiscordRPC.discordUpdatePresence(discordRichPresence);
+    }
+    public void setRichPresence(String state, String details, String bigImage, String bigImageText, String smallImage, String smallImageText){
+            discordRichPresence = new DiscordRichPresence.Builder(state)
+                    .setDetails(details)
+                    .setBigImage(bigImage, bigImageText)
+                    .setSmallImage(smallImage, smallImageText)
+                    .build();
+            DiscordRPC.discordUpdatePresence(discordRichPresence);
+    }
+
+    public DiscordRichPresence getDiscordRichPresence(){return discordRichPresence;}
+
+    public void updateRichPresence(){DiscordRPC.discordUpdatePresence(discordRichPresence);}
+
 }
