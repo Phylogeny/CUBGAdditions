d55c38fd1c16f1ca4ef9cf762d4f3c697f3a0786
ScottehBoeh <scottrobertson@btinternet.com>
2018-08-11 15:11:48 +0100

    - Added new render methods for image rendering - Fixed lang file not having proper Multiplayer/Singleplayer button translations

--- src/main/java/com/cubgdev/cubga/client/gui/GuiCubg.java
+++ src/main/java/com/cubgdev/cubga/client/gui/GuiCubg.java
@@ -76,7 +76,9 @@ public class GuiCubg extends GuiScreen {
     public void drawScreen(int mouseX, int mouseY, float partialTicks){
 
         GL11.glPushMatrix();
-        CUBGRenderHelper.renderImage(0,0,menuBackground,width,height);
+
+        CUBGRenderHelper.renderImageCentered(width / 2,0,menuBackground,width,height);
+
         CUBGRenderHelper.renderRectWithOutline(0,0,width,40,0x55000000,0x44000000,1);
         CUBGRenderHelper.renderRectWithOutline(0,height - 40,width,40,0x55000000,0x44000000,1);
         CUBGRenderHelper.renderImage(4,3,new ResourceLocation(Reference.MOD_ID,"textures/gui/logo.png"),110,29);
