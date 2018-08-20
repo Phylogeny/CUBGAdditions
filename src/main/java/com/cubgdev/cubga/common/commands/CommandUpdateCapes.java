7316003120d3c3009da34fc2d7b14b6a93d20039
Mastef_Chief <mastefchief@gmail.com>
2018-08-20 09:14:17 -0400

    fixed update capes command

--- src/main/java/com/cubgdev/cubga/common/commands/CommandUpdateCapes.java
+++ src/main/java/com/cubgdev/cubga/common/commands/CommandUpdateCapes.java
@@ -1,6 +1,8 @@
 package com.cubgdev.cubga.common.commands;
 
 import com.cubgdev.cubga.common.LootChestManager;
+import com.cubgdev.cubga.network.PacketHandler;
+import com.cubgdev.cubga.network.message.MessageUpdateCapes;
 import com.cubgdev.cubga.utils.cape.Capes;
 import net.minecraft.command.CommandBase;
 import net.minecraft.command.CommandException;
@@ -26,6 +28,6 @@ public class CommandUpdateCapes extends CommandBase {
     @Override
     public void execute(MinecraftServer server, ICommandSender sender, String[] args) throws CommandException
     {
-        Capes.update();
+        PacketHandler.INSTANCE.sendToAll(new MessageUpdateCapes(Capes.getCapes()));
     }
 }
