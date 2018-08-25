09e4713b1ecacf869950d6afc5f60b6a37ba1287
JacksonPlayz <jackson.hardaway@gmail.com>
2018-08-24 23:16:38 -0700

    Revert "fixed update capes command"
    This reverts commit 7316003120d3c3009da34fc2d7b14b6a93d20039.

--- src/main/java/com/cubgdev/cubga/common/commands/CommandUpdateCapes.java
+++ src/main/java/com/cubgdev/cubga/common/commands/CommandUpdateCapes.java
@@ -1,8 +1,6 @@
 package com.cubgdev.cubga.common.commands;
 
 import com.cubgdev.cubga.common.LootChestManager;
-import com.cubgdev.cubga.network.PacketHandler;
-import com.cubgdev.cubga.network.message.MessageUpdateCapes;
 import com.cubgdev.cubga.utils.cape.Capes;
 import net.minecraft.command.CommandBase;
 import net.minecraft.command.CommandException;
@@ -28,6 +26,6 @@ public class CommandUpdateCapes extends CommandBase {
     @Override
     public void execute(MinecraftServer server, ICommandSender sender, String[] args) throws CommandException
     {
-        PacketHandler.INSTANCE.sendToAll(new MessageUpdateCapes(Capes.getCapes()));
+        Capes.update();
     }
 }
