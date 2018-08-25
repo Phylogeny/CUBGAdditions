00454519045f512b4beb7d3e42ac3b6dbf84ba66
JacksonPlayz <jackson.hardaway@gmail.com>
2018-08-24 22:19:15 -0700

    Updated version.

--- src/main/java/com/cubgdev/cubga/Reference.java
+++ src/main/java/com/cubgdev/cubga/Reference.java
@@ -4,16 +4,16 @@ public class Reference
 {
     public static final String MOD_ID = "cubga";
     public static final String MOD_NAME = "CUBG Additions";
-    public static final String MOD_VERSION = "1.8.1";
+    public static final String MOD_VERSION = "1.9.0";
     public static final String MOD_COMPATIBILITY = "[1.12.2]";
-    public static final String MOD_DEPENDS = "required-after:obfuscate@[0.2.5,);required-after:cgm@[0.4.3,)";
+    public static final String MOD_DEPENDS = "required-after:obfuscate@[0.2.5,);required-after:cgm@[0.6.2,)";
 
     public static final String PROXY_CLIENT = "com.cubgdev.cubga.proxy.ClientProxy";
     public static final String PROXY = "com.cubgdev.cubga.proxy.CommonProxy";
     public static final String PROXY_SERVER = "com.cubgdev.cubga.proxy.ServerProxy";
 
     public static final String LINK_WEBSITE = "https://mrcrayfish.com/";
-    public static final String LINK_DISCORD = "http://discord.gg/gmcMRRk";
+    public static final String LINK_DISCORD = "https://discord.gg/gmcMRRk";
 
     public static final String SERVERIP_BEAVER = "cubg.mrcrayfish.com";
     public static final String SERVERIP_KANGAROO = "cubgau.mrcrayfish.com";
